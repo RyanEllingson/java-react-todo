a0632d24a056afe051b3b96f2c932b775ce92700
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-03-09 00:53:19 -0600

    implemented and tested all UserDatabaseRepository methods

--- src/main/java/com/ryan/data/UserDatabaseRepository.java
+++ src/main/java/com/ryan/data/UserDatabaseRepository.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.ryan.models.User;
 
@@ -15,7 +16,23 @@ public class UserDatabaseRepository implements UserRepository {
 	}
 
 	public int createUser(User user) {
-		return 0;
+		int insertId = 0;
+		String sql = "insert into users (first_name, last_name, email, password) values (?,?,?,?)";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, user.getFirstName());
+			ps.setString(2, user.getLastName());
+			ps.setString(3, user.getEmail());
+			ps.setString(4, user.getPassword());
+			ps.execute();
+			ResultSet rs = ps.getGeneratedKeys();
+			if (rs.next()) {
+				insertId = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return insertId;
 	}
 
 	public User getUserById(int userId) {
@@ -39,15 +56,53 @@ public class UserDatabaseRepository implements UserRepository {
 	}
 
 	public User getUserByEmail(String email) {
-		return null;
+		User user = new User();
+		String sql = "select user_id, first_name, last_name, email, password from users where email = ?";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, email);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				user.setUserId(rs.getInt(1));
+				user.setFirstName(rs.getString(2));
+				user.setLastName(rs.getString(3));
+				user.setEmail(rs.getString(4));
+				user.setPassword(rs.getString(5));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return user;
 	}
 
 	public int updateUser(User user) {
-		return 0;
+		int affectedRows = 0;
+		String sql = "update users set first_name = ?, last_name = ?, email = ?, password = ? where user_id = ?";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, user.getFirstName());
+			ps.setString(2, user.getLastName());
+			ps.setString(3, user.getEmail());
+			ps.setString(4, user.getPassword());
+			ps.setInt(5, user.getUserId());
+			affectedRows = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return affectedRows;
 	}
 
 	public int deleteUserById(int userId) {
-		return 0;
+		int affectedRows = 0;
+		String sql = "delete from users where user_id = ?";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, userId);
+			affectedRows = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return affectedRows;
 	}
 
 }
