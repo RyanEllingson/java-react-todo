3135960ad50ca7fca5196d0142c0027c11d7bf12
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-03-25 18:20:57 -0500

    configured JWTBuilder to use environment variable as signing key; wrote test for JWTBuilder

--- src/main/java/com/ryan/util/JWTBuilder.java
+++ src/main/java/com/ryan/util/JWTBuilder.java
@@ -4,16 +4,21 @@ import java.security.Key;
 
 import com.ryan.models.User;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 public class JWTBuilder {
-	private static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 	
 	public static String buildJWT(User user) {
+		Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(System.getenv("JWT_SECRET_STRING")));
 		String jws = Jwts.builder().setSubject(user.getEmail()).signWith(key).compact();
+		try {
+			System.out.println(Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jws).getBody().getSubject());
+		} catch (JwtException e) {
+			e.printStackTrace();
+		}
 		return jws;
 	}
-
 }
