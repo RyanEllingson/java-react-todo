cc490fd77b23b4c0f2860d1f74d9b7a077678e7b
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-03-25 18:23:54 -0500

    deleted unnecessary printout in JWTBuilder class

--- src/main/java/com/ryan/util/JWTBuilder.java
+++ src/main/java/com/ryan/util/JWTBuilder.java
@@ -4,7 +4,6 @@ import java.security.Key;
 
 import com.ryan.models.User;
 
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -14,11 +13,6 @@ public class JWTBuilder {
 	public static String buildJWT(User user) {
 		Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(System.getenv("JWT_SECRET_STRING")));
 		String jws = Jwts.builder().setSubject(user.getEmail()).signWith(key).compact();
-		try {
-			System.out.println(Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jws).getBody().getSubject());
-		} catch (JwtException e) {
-			e.printStackTrace();
-		}
 		return jws;
 	}
 }
