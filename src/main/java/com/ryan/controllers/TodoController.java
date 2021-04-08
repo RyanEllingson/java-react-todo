de78196ca71bca5cf65f8554d46f4b986a717482
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-04-07 19:49:27 -0500

    added api route to get todos for current user

--- src/main/java/com/ryan/controllers/TodoController.java
+++ src/main/java/com/ryan/controllers/TodoController.java
@@ -1,13 +1,38 @@
 package com.ryan.controllers;
 
+import java.io.IOException;
+import java.sql.Connection;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ryan.data.TodoDatabaseRepository;
+import com.ryan.data.UserDatabaseRepository;
+import com.ryan.service.TodoService;
+import com.ryan.util.ConnectionFactory;
+import com.ryan.util.JWTBuilder;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 
 public class TodoController {
 
 	public static void getTodos(HttpServletRequest req, HttpServletResponse res) {
-		System.out.println("inside getTodos");
-		System.out.println(req.getHeader("Authorization"));
-		res.setStatus(200);
+		Connection conn = ConnectionFactory.getConnection();
+		TodoService todoService = new TodoService(new TodoDatabaseRepository(conn), new UserDatabaseRepository(conn));
+		try {
+			ObjectMapper om = new ObjectMapper();
+			Jws<Claims> jws = JWTBuilder.parseJWS(req.getHeader("Authorization"));
+			if (jws == null) {
+				res.setStatus(401);
+			} else {
+				int userId = (int) jws.getBody().get("userId");
+				res.setStatus(200);
+				res.getWriter().write(om.writeValueAsString(todoService.getTodosByUser(userId)));
+			}
+		} catch (IOException e) {
+			res.setStatus(400);
+		}
 	}
 }
--- src/main/java/com/ryan/util/JWTBuilder.java
+++ src/main/java/com/ryan/util/JWTBuilder.java
@@ -7,6 +7,9 @@ import java.util.Date;
 
 import com.ryan.models.User;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -17,6 +20,20 @@ public class JWTBuilder {
 		Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(System.getenv("JWT_SECRET_STRING")));
 		Date expiration = new Date(Instant.now().plus(15, ChronoUnit.MINUTES).getEpochSecond() * 1000);
 		String jws = Jwts.builder().setSubject(user.getEmail()).setExpiration(expiration).claim("userId", user.getUserId()).claim("firstName", user.getFirstName()).claim("lastName", user.getLastName()).signWith(key).compact();
+		return jws;
+	}
+	
+	public static Jws<Claims> parseJWS(String jwsString) {
+		Jws<Claims> jws = null;
+		Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(System.getenv("JWT_SECRET_STRING")));
+		try {
+			jws = Jwts.parserBuilder()
+					.setSigningKey(key)
+					.build()
+					.parseClaimsJws(jwsString);
+		} catch (JwtException e) {
+			
+		}
 		return jws;
 	}
 }
