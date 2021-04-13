c92adbd6e1b2bdcf691e67ff75e8116e21f56e5a
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-04-12 19:31:54 -0500

    updated TodoCard styling

--- src/main/client/src/components/TodoCard/TodoCard.js
+++ src/main/client/src/components/TodoCard/TodoCard.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./TodoCard.css";
 
 const TodoCard = function({task, completed, todoId, deleteTodo, updateTodo}) {
     const handleDeleteTodo = function() {
         deleteTodo(todoId);
-    }
+    };
 
     const handleUpdateTodo = function() {
         updateTodo(todoId, task, !completed);
-    }
+    };
 
     return (
         <div className="TodoCard">
@@ -17,7 +18,7 @@ const TodoCard = function({task, completed, todoId, deleteTodo, updateTodo}) {
                         <p className="card-text">{task}</p>
                         <button type="button" className="btn-close" aria-label="delete" onClick={handleDeleteTodo}></button>
                     </div>
-                    <p className="card-text" onClick={handleUpdateTodo}>{completed ? "Completed" : "Not Completed"}</p>
+                    <p className="card-text completed-field" onClick={handleUpdateTodo}>{completed ? "Completed" : "Not Completed"}</p>
                 </div>
             </div>
         </div>
