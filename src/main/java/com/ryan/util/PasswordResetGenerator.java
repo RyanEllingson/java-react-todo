ade78ca515db688676957b2a0eaff82e5cfff936
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-04-20 20:46:40 -0500

    password reset code is now generated from letters, numbers, and special characters

--- src/main/java/com/ryan/util/PasswordResetGenerator.java
+++ src/main/java/com/ryan/util/PasswordResetGenerator.java
@@ -50,10 +50,11 @@ public class PasswordResetGenerator {
 	}
 	
 	private static String generateResetCode() {
+		String[] charOptions = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"};
 		String resetCode = "";
 		Random rand = new Random();
-		for (int i=0; i<6; i++) {
-			resetCode += rand.nextInt(10);
+		for (int i=0; i<10; i++) {
+			resetCode += charOptions[rand.nextInt(charOptions.length)];
 		}
 		return resetCode;
 	}
